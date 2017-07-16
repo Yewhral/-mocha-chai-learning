add7b8a60e2319acf0c8e9bab1fb3b9ce84f3770
Yewhral <zabrzyjewski@gmail.com>
2017-07-16 09:57:58 +0200

    Console log test information organized

--- test/appTest.js
+++ test/appTest.js
@@ -1,26 +1,29 @@
-const assert = require('chai').assert;
-//const sayHello = require('../app').sayHello;
-//const addNumbers = require('../app').addNumbers;
+const assert = require('chai').assert;;
 const app = require("../app");
 
 const sayHelloResult = app.sayHello();
 const addNumbersResult = app.addNumbers(5,10);
 
 describe('App',function(){
-    it('sayHello should say hello', () => {
-        assert.equal(sayHelloResult,'hello');
+
+    describe('sayHello()', () => {
+        it('sayHello should say hello', () => {
+            assert.equal(sayHelloResult, 'hello');
+        });
+
+        it('sayHello should return a string', () => {
+            assert.typeOf(sayHelloResult, 'string');
+        });
     });
 
-    it('sayHello should return a string', () => {
-        assert.typeOf(sayHelloResult, 'string');
-    });
+    describe('addNumbers()', () => {
+        it('addNumbers should return value higher than 10', () => {
+            assert.isAbove(addNumbersResult, 10);
+        });
 
-    it('addNumbers should return value higher than 10', () => {
-        assert.isAbove(addNumbersResult, 10);
-    });
-
-    it('addNumbers should return a number', () => {
-        assert.typeOf(addNumbersResult, 'number');
+        it('addNumbers should return a number', () => {
+            assert.typeOf(addNumbersResult, 'number');
+        });
     });
 
 });
