a188cfb2ccacbb02355a5c1581342721c0e1b505
Yewhral <zabrzyjewski@gmail.com>
2017-07-16 09:40:12 +0200

    Basic improvements to unit tests

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,17 @@
 const assert = require('chai').assert;
+const sayHello = require('../app').sayHello;
 const app = require("../app");
 
 describe('App',function(){
-    it('app should say hello', function(){
-       assert.equal(app(),'hello');
+    it('app should say hello', function() {
+        let result = sayHello();
+       assert.equal(result,'hello');
     });
+
+    it('app should return a string', () => {
+        let result = sayHello();
+        assert.typeOf(result, 'string');
+        }
+    )
+
 });
