9efc173c355324262ef78fc04381c69c32438323
Yewhral <zabrzyjewski@gmail.com>
2017-07-16 09:53:31 +0200

    Better unit test organisation

--- test/appTest.js
+++ test/appTest.js
@@ -1,17 +1,27 @@
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
-const app = require("../app");
+const addNumbers = require('../app').addNumbers;
+//const app = require("../app");
 
 describe('App',function(){
-    it('app should say hello', function() {
+    it('sayHello should say hello', () => {
         let result = sayHello();
-       assert.equal(result,'hello');
+        assert.equal(result,'hello');
     });
 
-    it('app should return a string', () => {
+    it('sayHello should return a string', () => {
         let result = sayHello();
         assert.typeOf(result, 'string');
-        }
-    )
+    });
+
+    it('addNumbers should return value higher than 10', () => {
+        let result = addNumbers(10,10);
+        assert.isAbove(result, 10);
+    });
+
+    it('addNumbers should return a number', () => {
+        let result = addNumbers(0,10);
+        assert.typeOf(result, 'number');
+    });
 
 });
